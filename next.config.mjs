2987d11cea3a1035af43d255b4afa7b8d56a0ba6
irvan saharudin <irvansaharudin@gmail.com>
2024-01-25 12:15:51 +0700

    add sass and icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
 
 export default nextConfig;
