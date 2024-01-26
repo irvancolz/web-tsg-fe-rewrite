3cb4848de07fe8997bac853f19da56016f58d381
irvan saharudin <irvansaharudin@gmail.com>
2024-01-26 20:59:31 +0700

    add fallback to underdeveloped page

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,60 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  async redirects() {
+    return [
+      {
+        source: "/about-us",
+        destination: "/under-construction",
+        permanent: true,
+      },
+      {
+        source: "/our-services",
+        destination: "/under-construction",
+        permanent: true,
+      },
+      {
+        source: "/contact",
+        destination: "/under-construction",
+        permanent: true,
+      },
+      {
+        source: "/blogs",
+        destination: "/under-construction",
+        permanent: true,
+      },
+      {
+        source: "/tailor-made-application-development",
+        destination: "/under-construction",
+        permanent: true,
+      },
+      {
+        source: "/technology-managed-services",
+        destination: "/under-construction",
+        permanent: true,
+      },
+      {
+        source: "/it-manpower-supply",
+        destination: "/under-construction",
+        permanent: true,
+      },
+      {
+        source: "/it-roadmap-and-strategic-planning",
+        destination: "/under-construction",
+        permanent: true,
+      },
+      {
+        source: "/software-qa-and-security-testing",
+        destination: "/under-construction",
+        permanent: true,
+      },
+      {
+        source: "/agile-and-scrum-training-consulting",
+        destination: "/under-construction",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
