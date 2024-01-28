7ea045dbf493be56f8c0cf3597c915ae7a4a9e43
irvan saharudin <irvansaharudin@gmail.com>
2024-01-28 14:19:50 +0700

    add link to chat pd

--- src/components/page-sections/home/project-director-words/pd-words.tsx
+++ src/components/page-sections/home/project-director-words/pd-words.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import style from "./pd-words.module.scss";
 import { Images } from "@/components";
 import { getStaticAssetsPath } from "@/consts";
+import Link from "next/link";
 
 export function ProjectDirectorWords() {
   return (
@@ -17,11 +18,18 @@ export function ProjectDirectorWords() {
         </blockquote>
       </div>
       <div className={style.pd}>
-        <Images
-          className={style.pd_img}
-          alt="Mr faiza pict"
-          src={getStaticAssetsPath("/images/webp", "pak faiza.webp")}
-        />
+        <Link
+          href={
+            "https://wa.me/62811831357?text=Saya%20ingin%20berkolaborasi%20dengan%20TSG"
+          }
+          target="_blank"
+        >
+          <Images
+            className={style.pd_img}
+            alt="Mr faiza pict"
+            src={getStaticAssetsPath("/images/webp", "pak faiza.webp")}
+          />
+        </Link>
         <p className={style.pd_name}>Faiza Renaldi</p>
         <p className={style.pd_title}>Project Director at TSG.</p>
       </div>
