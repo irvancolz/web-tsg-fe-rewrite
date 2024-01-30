b85f67131d97153baf5f38f769da0abdbcea13e8
irvan saharudin <irvansaharudin@gmail.com>
2024-01-30 11:43:58 +0700

    add icon and company title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,18 @@ export default function Layout({ children }: { children: ReactNode }) {
   }, []);
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="shortcut icon"
+          href="/images/png/icon_tsg.png"
+          type="image/x-icon"
+        />
+        <link rel="icon" sizes="16x16" href="/images/png/icon_tsg.png" />
+        <title>PT Tristar Surya Gemilang</title>
+      </head>
       <body>
         <Topnav />
         <main>
