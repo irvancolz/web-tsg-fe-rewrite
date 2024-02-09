aa3ee57f1ea6b3f29f1608d913a8779ba7912f95
irvan saharudin <irvansaharudin@gmail.com>
2024-02-09 11:31:15 +0700

    change icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,12 +34,8 @@ export default function Layout({ children }: { children: ReactNode }) {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          rel="shortcut icon"
-          href="/images/png/icon_tsg.png"
-          type="image/x-icon"
-        />
-        <link rel="icon" sizes="16x16" href="/images/png/icon_tsg.png" />
+        <link rel="shortcut icon" href="/icon_tsg.png" type="image/x-icon" />
+        <link rel="icon" sizes="16x16" href="/icon_tsg.png" />
         <title>PT Tristar Surya Gemilang</title>
       </head>
       <body>
