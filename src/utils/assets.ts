360e412ecbfc516b315bcb9e038b8a9d7129a047
irvan saharudin <irvansaharudin@gmail.com>
2024-02-23 21:53:06 +0700

    change getSupabasePublicUrl to getDynamicAssetsUrl

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,6 +1,6 @@
 import { getSupabasePublicUrl } from "@/api/supabase";
 
 export function getDynamicAssetsUrl(url: string): string {
-  if (url.startsWith("/")) return getSupabasePublicUrl(url);
+  if (!url.startsWith("../")) return getSupabasePublicUrl(url);
   return url;
 }
