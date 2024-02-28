ef15807674efd202102d836cad638238b476e0ea
irvan saharudin <irvansaharudin@gmail.com>
2024-02-28 13:35:23 +0700

    update get dynamic assets to support blob file

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,6 +1,7 @@
 import { getSupabasePublicUrl } from "@/api/supabase";
 
 export function getDynamicAssetsUrl(url: string): string {
-  if (url.startsWith("/")) return getSupabasePublicUrl(url);
+  if (!url.startsWith("http") && !url.startsWith("blob"))
+    return getSupabasePublicUrl(url);
   return url;
 }
