352827b376990117a19704063ae51c40a2f7e3a5
irvan saharudin <irvansaharudin@gmail.com>
2024-03-07 20:52:12 +0700

    update bucket policies

--- supabase/migrations/20240307133922_create_bucket.sql
+++ supabase/migrations/20240307133922_create_bucket.sql
@@ -1,5 +1,5 @@
 insert into storage.buckets (id, name, public)
 values ('tsg-blog-assets', 'tsg-blog-assets', true);
 create policy "Allow authenticated uploads" on storage.objects for all to authenticated with check (bucket_id = 'tsg-blog-assets');
-create policy "Allow everybody to access" on storage.objects for
-select to anon
+CREATE POLICY "allow everyone to view assets" ON storage.objects FOR
+SELECT TO public USING (bucket_id = 'tsg-blog-assets');
